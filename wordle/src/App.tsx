2ae2410bd963ce8b9eb4f7bcc7c862dec89b5450
momi329 <amekaka@gmail.com>
2023-03-14 14:16:28 +0800

    Add wordle page

--- wordle/src/App.tsx
+++ wordle/src/App.tsx
@@ -1,7 +1,189 @@
 import React from "react";
 
 function App() {
-  return <h1 className='text-green-700 bg-green-500'>hello world</h1>;
+  return (
+    <>
+      <h1 className='text-green-700 bg-green-500 text-center m-6'>
+        MACY'S WORDLE
+      </h1>
+      <div className='flex flex-row justify-center'>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-darkGray bg-darkGray text-white'
+        >
+          O
+        </div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-green bg-green text-white'
+        >
+          U
+        </div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-darkGray bg-darkGray text-white'
+        >
+          T
+        </div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-darkGray bg-darkGray text-white'
+        >
+          E
+        </div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-yellow bg-yellow text-white'
+        >
+          R
+        </div>
+      </div>
+      <div className='flex flex-row justify-center'>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-darkGray bg-white text-black'
+        >
+          A
+        </div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-darkGray bg-white text-black'
+        >
+          E
+        </div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-gray bg-white text-black'
+        ></div>
+      </div>
+      <div className='flex flex-row justify-center'>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+        m-1 flex items-center justify-center
+        border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+      </div>
+      <div className='flex flex-row justify-center'>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+        m-1 flex items-center justify-center
+        border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+      </div>
+      <div className='flex flex-row justify-center'>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+        m-1 flex items-center justify-center
+        border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+      </div>
+      <div className='flex flex-row justify-center'>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+            m-1 flex items-center justify-center
+            border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+        m-1 flex items-center justify-center
+        border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+        <div
+          className='box-border w-16 h-16 border-solid border-2
+          m-1 flex items-center justify-center
+          border-gray bg-white text-black'
+        ></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- wordle/tailwind.config.js
+++ wordle/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#6aaa64",
+        yellow: "#c9b458",
+        darkGray: "#787c7e",
+        gray: "#d3d6da",
+        black: "#000000",
+        white: "#ffffff",
+      },
+    },
   },
   plugins: [],
 };
