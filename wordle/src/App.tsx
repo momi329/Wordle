591b83f513cedbd7be7d2586bfdcb7f0001cc038
momi329 <amekaka@gmail.com>
2023-03-14 17:17:34 +0800

    phase2

--- wordle/src/App.tsx
+++ wordle/src/App.tsx
@@ -1,186 +1,171 @@
 import React from "react";
+import { useState, useRef, useReducer } from "react";
+
+const allData = [
+  [
+    { letter: "I", state: "green" },
+    { letter: "D", state: "yellow" },
+    { letter: "I", state: "gray" },
+    { letter: "O", state: "yellow" },
+    { letter: "T", state: "yellow" },
+  ],
+  [
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+  ],
+  [
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+  ],
+  [
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+  ],
+  [
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+  ],
+  [
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+    { letter: "", state: "" },
+  ],
+];
+const result = [...allData];
+console.log(result);
+// const isMatched = (inputLine: number) => {
+//   answer.map((element, index) => {
+//     const result = [...allData];
+//     console.log(result);
+//     if (element === matchingData[index].toUpperCase()) {
+//       return (result[inputLine][index].state = "green");
+//     } else if (answer.includes(matchingData[index].toUpperCase())) {
+//       return (result[inputLine][index].state = "yellow");
+//     } else {
+//       return (result[inputLine][index].state = "gray");
+//     }
+//   });
+// };
+// console.log(result);
+const inputStyle =
+  "box-border w-16 h-16 border-solid border-2 m-1 flex items-center justify-center border-gray bg-white text-black text-center";
 
 function App() {
+  //const [inputLine, setInputLine] = useState(0);
+  // const [inputArr, setInputArr] = useState([]);
+  // const [allData, dispatch] = useReducer(reducer, [
+  //   [
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //   ],
+  //   [
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //   ],
+  //   [
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //   ],
+  //   [
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //   ],
+  //   [
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //   ],
+  //   [
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //     { letter: "", state: "" },
+  //   ],
+  // ]);
+
+  const inputRef = useRef(null);
   return (
     <>
       <h1 className='text-green-700 bg-green-500 text-center m-6'>
         MACY'S WORDLE
       </h1>
-      <div className='flex flex-row justify-center'>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-darkGray bg-darkGray text-white'
-        >
-          O
-        </div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
+      <div className='flex flex-row justify-center flex-wrap w-1/2 m-auto'>
+        {allData.map((element, index) => {
+          return element.map((item, num) => {
+            if (item.state === "" && item.letter === "") {
+              return <input type='text' className={inputStyle}></input>;
+            } else if (item.state === "" && item.letter !== "") {
+              return (
+                <div
+                  className='box-border w-16 h-16 border-solid border-2
+              m-1 flex items-center justify-center
+              border-darkGray bg-white text-black'
+                >
+                  {item.letter}
+                </div>
+              );
+            } else if (item.state === "green") {
+              return (
+                <div
+                  className='box-border w-16 h-16 border-solid border-2
             m-1 flex items-center justify-center
             border-green bg-green text-white'
-        >
-          U
-        </div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-darkGray bg-darkGray text-white'
-        >
-          T
-        </div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-darkGray bg-darkGray text-white'
-        >
-          E
-        </div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-yellow bg-yellow text-white'
-        >
-          R
-        </div>
-      </div>
-      <div className='flex flex-row justify-center'>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-darkGray bg-white text-black'
-        >
-          A
-        </div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-darkGray bg-white text-black'
-        >
-          E
-        </div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-gray bg-white text-black'
-        ></div>
-      </div>
-      <div className='flex flex-row justify-center'>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
+                >
+                  {item.letter}
+                </div>
+              );
+            } else if (item.state === "yellow") {
+              return (
+                <div
+                  className='box-border w-16 h-16 border-solid border-2
         m-1 flex items-center justify-center
-        border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-      </div>
-      <div className='flex flex-row justify-center'>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
+        border-yellow bg-yellow text-white'
+                >
+                  {item.letter}
+                </div>
+              );
+            } else if (item.state === "gray") {
+              return (
+                <div
+                  className='box-border w-16 h-16 border-solid border-2
         m-1 flex items-center justify-center
-        border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-      </div>
-      <div className='flex flex-row justify-center'>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-        m-1 flex items-center justify-center
-        border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-      </div>
-      <div className='flex flex-row justify-center'>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-            m-1 flex items-center justify-center
-            border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-        m-1 flex items-center justify-center
-        border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
-        <div
-          className='box-border w-16 h-16 border-solid border-2
-          m-1 flex items-center justify-center
-          border-gray bg-white text-black'
-        ></div>
+        border-darkGray bg-darkGray text-white'
+                >
+                  {item.letter}
+                </div>
+              );
+            }
+          });
+        })}
       </div>
     </>
   );
