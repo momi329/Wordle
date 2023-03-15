b47c0cfe62beac805cdd64346ca3b2c2a2330a2b
momi329 <amekaka@gmail.com>
2023-03-15 18:26:45 +0800

    Almost complete

--- wordle/src/App.tsx
+++ wordle/src/App.tsx
@@ -1,119 +1,78 @@
 import React from "react";
-import { useState, useRef, useReducer } from "react";
+import { useState, useRef, useEffect } from "react";
+const answer = ["H", "A", "P", "P", "Y"];
+const stateClassName: { [key: string]: string } = {
+  gray: " border-darkGray bg-darkGray text-white",
+  green: " border-green bg-green text-white",
+  yellow: " border-yellow bg-yellow text-white",
+  typing: " border-darkGray bg-white text-black",
+  nothing:
+    "box-border w-16 h-16 border-solid border-2 m-1 flex items-center justify-center border-gray bg-white text-black text-center",
+  boxStyle:
+    "box-border w-16 h-16 border-solid border-2 m-1 flex items-center justify-center",
+};
 
-const allData = [
-  [
-    { letter: "I", state: "green" },
-    { letter: "D", state: "yellow" },
-    { letter: "I", state: "gray" },
-    { letter: "O", state: "yellow" },
-    { letter: "T", state: "yellow" },
-  ],
-  [
-    { letter: "B", state: "typing" },
-    { letter: "B", state: "typing" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-  ],
-  [
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-  ],
-  [
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-  ],
-  [
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-  ],
-  [
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-    { letter: "", state: "" },
-  ],
-];
-const result = [...allData];
-console.log(result);
-// const isMatched = (inputLine: number) => {
-//   answer.map((element, index) => {
-//     const result = [...allData];
-//     console.log(result);
-//     if (element === matchingData[index].toUpperCase()) {
-//       return (result[inputLine][index].state = "green");
-//     } else if (answer.includes(matchingData[index].toUpperCase())) {
-//       return (result[inputLine][index].state = "yellow");
-//     } else {
-//       return (result[inputLine][index].state = "gray");
-//     }
-//   });
-// };
-// console.log(result);
-const inputStyle =
-  "box-border w-16 h-16 border-solid border-2 m-1 flex items-center justify-center border-gray bg-white text-black text-center";
-const boxStyle =
-  "box-border w-16 h-16 border-solid border-2 m-1 flex items-center justify-center";
 function App() {
-  //const [inputLine, setInputLine] = useState(0);
-  // const [inputArr, setInputArr] = useState([]);
-  // const [allData, dispatch] = useReducer(reducer, [
-  //   [
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //   ],
-  //   [
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //   ],
-  //   [
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //   ],
-  //   [
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //   ],
-  //   [
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //   ],
-  //   [
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //     { letter: "", state: "" },
-  //   ],
-  // ]);
+  const [currentLine, setCurrentLine] = useState<boolean[]>([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const inputRefs = useRef<any[]>([]);
+  const [allData, setAllData] = useState<string[][]>([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ]);
 
-  //const inputRef = useRef(null);
+  const handleGuessChange = (index: number, num: number, input: string) => {
+    console.log(input);
+    const newData = [...allData];
+    newData[index][num] = input.toUpperCase();
+    setAllData(newData);
+    if (input !== "" && num < answer.length + 1) {
+      return num + 1 === 5
+        ? inputRefs.current[index]
+        : inputRefs.current[index][num + 1].focus();
+    }
+  };
+  const keyChange = (
+    index: number,
+    num: number,
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    //輸入 退回 提交
+    //輸入
+    if (event.key === "Enter") {
+      if (allData[index].every((i: string) => i !== "")) {
+        setCurrentLine((prev) => {
+          const newState = [...prev];
+          newState[index] = true;
+          return newState;
+        });
+        inputRefs.current[index + 1][0].focus();
+      }
+    } else if (event.key === "Backspace") {
+      const newData = [...allData];
+      console.log(`${index} ${num}`);
+      newData[index][num] = "";
+      setAllData(newData);
+      if (num > 0) {
+        //inputRefs.current[index][num].current.blur();
+        inputRefs.current[index][num - 1].focus();
+      }
+      console.log("deleted");
+    }
+  };
+  useEffect(() => {
+    inputRefs.current[0][0]?.focus();
+  }, []);
   return (
     <>
       <h1 className='text-green-700 bg-green-500 text-center m-6'>
@@ -121,47 +80,61 @@ function App() {
       </h1>
       <div className='flex flex-row justify-center flex-wrap w-96 m-auto'>
         {allData.map((element, index) => {
+          let stateClass: string = "";
           return element.map((item, num) => {
-            switch (item.state) {
-              case "":
-                return <input type='text' className={inputStyle} />;
-
-              case "typing":
-                return (
-                  <div
-                    className={`${boxStyle} border-darkGray bg-white text-black`}
-                  >
-                    {item.letter}
-                  </div>
-                );
-
-              case "green":
-                return (
-                  <div
-                    className={`${boxStyle} border-green bg-green text-white`}
-                  >
-                    {item.letter}
-                  </div>
-                );
-
-              case "yellow":
-                return (
-                  <div
-                    className={`${boxStyle} border-yellow bg-yellow text-white`}
-                  >
-                    {item.letter}
-                  </div>
-                );
-
-              case "gray":
-                return (
-                  <div
-                    className={`${boxStyle} border-darkGray bg-darkGray text-white`}
-                  >
-                    {item.letter}
-                  </div>
-                );
+            if (item === "" && !currentLine[index]) {
+              stateClass = stateClassName.nothing;
+            } else if (item !== "" && !currentLine[index]) {
+              stateClass = stateClassName.typing;
+            } else if (item === answer[num] && currentLine[index]) {
+              stateClass = stateClassName.green;
+            } else if (
+              item !== answer[num] &&
+              answer.includes(item) &&
+              currentLine[index]
+            ) {
+              stateClass = stateClassName.yellow;
+            } else {
+              stateClass = stateClassName.gray;
             }
+            return (
+              <div
+                key={`${index}-${num}`}
+                className={`${stateClassName.boxStyle} ${stateClass}`}
+              >
+                {currentLine[index] === false || item === "" ? (
+                  <input
+                    type='text'
+                    maxLength={1}
+                    key={`${index}-${num}`}
+                    className={`${stateClassName.boxStyle} ${stateClass}`}
+                    ref={(input) => {
+                      if (!inputRefs.current[index]) {
+                        inputRefs.current[index] = [];
+                      }
+                      inputRefs.current[index][num] = input;
+                    }}
+                    onChange={(e) => {
+                      const rule: RegExp = /^[A-Za-z]{1}$/;
+                      if (rule.test(e.target.value)) {
+                        handleGuessChange(index, num, e.target.value);
+                        e.target.value = e.target.value.toUpperCase();
+                      }
+                    }}
+                    onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                      keyChange(index, num, e);
+                    }}
+                    value={item}
+                  />
+                ) : (
+                  <input
+                    value={item}
+                    className={`${stateClassName.boxStyle} ${stateClass}`}
+                    readOnly={currentLine[index]}
+                  />
+                )}
+              </div>
+            );
           });
         })}
       </div>
